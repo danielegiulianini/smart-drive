79abb09f13cd5727440a6eba0eb3d8127c6ca929
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-08-27 16:44:20 +0200

    docker setup mqtt broker and users microservice for with reverse proxy complete

--- backend/users/index.js
+++ backend/users/index.js
@@ -1,12 +1,17 @@
 // Require express and create an instance of it
-var express = require('express');
+var express = require("express");
 var app = express();
 
-console.log("binding routes...");
-
-
 // on the request to root (localhost:3000/)
-app.get('/', function (req, res) {
-   console.log("Ricevuto una richiesta GET");
-   res.send('<b>My</b> first express http server');
+app.get("/user/", function (req, res) {
+  console.log("Ricevuto una richiesta GET");
+  //res.send("<b>My</b> first express http server");
 });
+
+console.log("routes bound");
+
+
+const port = Number(process.env.USERS_MICROSERVICE_INTERNAL_PORT || 8083)
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
