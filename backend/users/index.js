167d72fb465158a13cc92b67b928bf61088ec1a8
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-08-27 11:20:44 +0200

    setup dependencies for users microservice

---
+++ backend/users/index.js
@@ -0,0 +1,9 @@
+// Require express and create an instance of it
+var express = require('express');
+var app = express();
+
+// on the request to root (localhost:3000/)
+app.get('/', function (req, res) {
+   console.log("Ricevuto una richiesta GET");
+    res.send('<b>My</b> first express http server');
+});
