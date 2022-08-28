30e8b4d3d1c0b393d0c97d9c83a6b5df0af392ac
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-08-28 10:23:02 +0200

    add setup for trips microservice

---
+++ backend/trips/db.config.js
@@ -0,0 +1,29 @@
+const username = process.env.TRIPS_DB_ROOT_USERNAME;
+const password = process.env.TRIPS_DB_ROOT_PASSWORD;// || 'users2022';
+const host = "MongoUsers";
+const port = process.env.TRIPS_DB_MONGO_INTERNAL_PORT// || 27017; //default mongodb port
+const defaultAuthDb = 'admin';
+const options = [
+    'retryWrites=true',
+    'w=majority',
+];
+
+/*
+* URI Format : mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
+* Source: https://docs.mongodb.com/manual/reference/connection-string/
+*/
+//include port in connection string (if defined).
+//if not defined: enable dns lookup with +srv
+
+function getConnectionUri(port, username, password, host, defaultAuthDb, options){
+    return `mongodb${port ? '' : '+srv'}://${username}:${password}@${host}${port ? ':' + port : ''}/${defaultAuthDb}?${options.join('&')}`;
+}//if refactoring: const getConnectionUri = require('../../sharedUtils/mongoDbUtils.js');
+
+
+connectionUri = getConnectionUri(port, username, password, host, defaultAuthDb, options);
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><il connection uri is: " + connectionUri + ">>>>>>>>>>>>>>>>>>>>>>>>>>><");
+
+module.exports = {
+    connectionUri,
+}
