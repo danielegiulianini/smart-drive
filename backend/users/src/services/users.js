fe12e095a3a80afb4fb8079b727fd5ed88ce19d9
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-08-28 22:14:03 +0200

    add services for users microservice

---
+++ backend/users/src/services/users.js
@@ -0,0 +1,50 @@
+const Profile = require("../models/users");
+
+const list = () => {
+  return Profile.find();
+};
+
+const add = (profile) => {
+  console.log(`Adding profile: ${JSON.stringify(profile)}`);
+  return Profile.findOne({
+    _id: profile._id,
+  })
+    .then((result) => {
+      if (result) {
+        throw new TypeError(`The profile ${profile._id} already exists`);
+      }
+    })
+    .then(() => Profile.create(profile));
+};
+
+const get = (userId) => {
+  console.log(`Getting profile by ID: ${userId}`);
+  return Profile.findById(userId);
+};
+
+const edit = (userId, params) => {
+  console.log(`Updating user ${userId} with data: ${JSON.stringify(params)}`);
+  return Profile.findOneAndUpdate(
+    {
+      _id: userId,
+    },
+    params,
+    {
+      new: true,
+    }
+  );
+};
+
+const remove = (userId) => {
+  return Profile.deleteOne({
+    _id: userId,
+  });
+};
+
+module.exports = {
+  list,
+  add,
+  get,
+  edit,
+  remove,
+};
