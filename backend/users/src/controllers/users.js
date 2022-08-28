b879c2c4b000c11063e4e01919ec3603a6fa04bf
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-08-28 22:39:45 +0200

    add controller for users

---
+++ backend/users/src/controllers/users.js
@@ -0,0 +1,45 @@
+const profileService = require("../services/users");
+
+const create = async (req, res) => {
+  //params contained in client's post request
+  let params = {
+    _id: req.body.tokenUserId,
+    surname: req.body.surname,
+    name: req.body.name,
+    gender: req.body.gender,
+    language: req.body.language,
+    email: req.body.email,
+    city: req.body.city,
+    country: req.body.country,
+  };
+
+  profileService
+    .add(params)
+    .then((profile) => res.status(201).json(profile))   //todo actions to be refactored
+    .catch((err) => res.status(400).json(err));
+};
+
+const get = (req, res) => {
+  const userId = req.params.userId;
+
+  profileService
+    .get(userId)
+    .then((profile) => res.status(200).json(profile))
+    .catch((err) => res.status(400).json(err));
+};
+
+const edit = (req, res) => {
+  const userId = req.params.userId;
+  const params = req.body;
+
+  profileService
+    .edit(userId, params)
+    .then((profile) => res.status(200).json(profile))
+    .catch((err) => res.status(400).json(err));
+};
+
+module.exports = {
+  create,
+  get,
+  edit,
+};
