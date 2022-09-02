b34f6d2779c218c2e36303de86d4ade24dc6e450
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-09-02 15:38:36 +0200

    setup images server

---
+++ backend/images/index.js
@@ -0,0 +1,44 @@
+//not actually needed (since not copied .env file into container)
+//require('dotenv').config({ path: require('find-config')('.env'), debug: true }); //require('dotenv').config({path:__dirname + '/./../../../.env'});
+
+const connectionUri = require("./src/config/db.config.js");
+
+// Require express and create an instance of it
+var express = require("express");
+var app = express();
+
+//cors library abstracts header-writing
+const cors = require("cors");
+const corsOptions = {
+  origin: true, //set origin to true to reflect the request origin (stricter than wildcard *) so browsers allow to view response
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+
+//body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const port = 8086; //const port = Number(process.env.USERS_MICROSERVICE_INTERNAL_PORT);
+
+//retrieve connection string from config file (.env or inside config folder)
+const dbConfig = require("./src/config/db.config");
+const dbUtil = require("./src/utils/mongooseUtils");
+
+async function startServer() {
+  //no need for db...
+  console.log("Connecting to db...");
+  await dbUtil.connect(dbConfig);
+  console.log("Connected!");
+
+  console.log("Setting up routes ...");
+  const routes = require("./routes");
+
+  /*base micro path*/
+  app.use("/api/v1", routes);
+  console.log("routes bound");
+
+  app.listen(port, () => console.log(`Server listening on port ${port}!`));
+}
+
+startServer();
