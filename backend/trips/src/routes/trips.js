f051b6a105eb6f6c2dc097ef1317f45dca090fae
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-10-03 10:14:32 +0200

    fix trips model

--- backend/trips/src/routes/trips.js
+++ backend/trips/src/routes/trips.js
@@ -5,38 +5,42 @@ const AuthMiddleware = require("./../middlewares/auth");
 const TripsController = require("../controllers/trips");
 
 //TODO: instead of the controller's handler (directly) could bind to the path an action method that
-//does some prints (so removing them from tha handlers)
+//does some prints (so removing them from the handlers)
 
-//routes' base url: images/api/v1/
+//routes' base url: trips/api/v1/
 
 router
   .use(AuthMiddleware.extractUserIdFromTokenAndPutItToBody)
   .route("/")
+  //here calling validators
   .get(TripsController.getAll);
 
 //get single trip? yes, for detail
 router
   .use(AuthMiddleware.extractUserIdFromTokenAndPutItToBody)
   .route("/:id")
+  //here calling validators
+
   .get(TripsController.get);
 
+//write API
 router
   .use(AuthMiddleware.extractUserIdFromTokenAndPutItToBody) //set up middleware for post too? yes, because with supabase you have set it already
   .route("/")
+  //here calling validators
+
   .post(TripsController.create);
 
-//should prefer a PUT (idempotency gratis, but cors complicated)
+//order of preference: PATCH (no need in REST to send all the resource but cors complicated), PUT (idempotency gratis but cors
+//complicated). So, post.
 router
   .use(AuthMiddleware.extractUserIdFromTokenAndPutItToBody) //set up middleware for post too? yes, because with supabase you have set it already
-  .route("/")
-  .post(TripsController.create);
+  .route("/:tripId")
+  //here calling validators
+
+  .post(TripsController.close);
+//close is a verb! not rest!
 
 //delete?
 
 module.exports = router;
-
-/* alternative syntax:
-router.post('/signup',userControllers.userRegister);
-router.get('/me', checkAuth, userControllers.getMe);
-router.get('/me', checkAuth, userControllers.getMe);
-*/
