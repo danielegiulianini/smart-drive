fe2b9ac451f3f170f5ba03f3174830618b809a38
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-10-05 12:31:10 +0200

    complete badging system

--- backend/trips/src/services/trips.achievements.js
+++ backend/trips/src/services/trips.achievements.js
@@ -28,31 +28,12 @@ const getAchievements = (trip) => {
   //===================================================
 };
 
-//============encouraging app usage=================
-/*const weeksDrivenInARow = (tripsOfUser) => {
-  //not trivial: per ogni week controllo se quella prec è la sua precedente?
-  groupBy(tripsOfUser, (trip) => getWeekNumber(new Date())).length;
-};*/
-
 const monthsDrivenInAYear = (userId) => {
-  /*groupBy(
-    groupBy(tripsOfUser, (trip) => [
-      trip.startTimestamp.getMonth(),
-      trip.startTimestamp.getYear(),
-    ]),
-    (monthAndYear) => monthAndYear[1]
-  );*/
-
-  //alternative with mongoose + tripId
-  const tripId = 2;
-  //group by month year
-  //group by year
   const monthsDrivenInYears = Trip.aggregate([
     { $match: { userId: userId } }, //filter only data of requested trip
     //{ $match: { timestamp: { $gte: dateStart } } }, //filter only data inside sliding window
     {
       $group: {
-        //1st grouping (in temporal window) for getting individual (window) metric
         _id: {
           year: { $year: "$startTimestamp" }, //group by expression
           month: { $year: "$startTimestamp" },
@@ -77,44 +58,135 @@ const monthsDrivenInAYear = (userId) => {
   return monthsDrivenInYears ? monthsDrivenInYears[0] : 0;
 };
 
-const tripsInAWeek = (userId) => {
-  //filtro quelli da una settimana fa as ora... o tutti ?? posso fare anche tutti tanto i documents sono pochi...
-  //raggruppo per week e per ognuna ho le drives
-  //groupBy(trip.measurements, (trip) => getWeekNumber(trip.startTimestamp));
-
-  const tripsPerWeek = Trip.aggregate([
-    { $match: { userId: userId } }, //filter only data of requested trip
-    //{ $match: { timestamp: { $gte: dateStart } } }, //filter only data inside sliding window
-    {
-      $group: {
-        _id: {
-          week: { $week: "$startTimestamp" }, //group by expression
-        },
-        //summary fields
-        tripsPerWeek: { $sum: 1 },
-      },
-    },
-    { $sort: { tripsPerWeek: -1 } },
-  ]);
-
-  return tripsPerWeek ? tripsPerWeek[0] : 0;
+const fetchFuelType = async (vin) => {
+  userVehiclesUrl = "http://vehicles:8087/api/v1/userVehicles/" + vin; //read from config files
+  const response = await axios.get(userVehiclesUrl, {});
+  vehiclesModelUrl =
+    "http://vehicles:8087/api/v1/vehiclesModels/vehicleDetails/" +
+    response.data.vehicleId;
+  const vehicleDetails = await axios.get(vehiclesModelUrl, {});
+  return vehicleDetails.fuelType + vehicleDetails.fuelType1;
 };
 
-const tripsInADay = (trip) => {
-  const tripsPerDay = Trip.aggregate([
-    { $match: { userId: userId } }, //filter only data of requested trip
+const performancesInInterval = async (
+  userIdAsKeyValueObject,
+  IntervalAsKeyValueObject
+) => {
+  //i risultati vanno poi ordinati secondo la prospettiva scelta
+
+  return Trip.aggregate([
+    { $match: { userId: userId } }, //filter only data of requested user
     {
       $group: {
-        _id: { $dateToString: { format: "%Y-%m-%d", date: "$at" } },
-        tripsPerDay: { $sum: 1 },
+        _id: Object.assign(userIdAsKeyValueObject, IntervalAsKeyValueObject),
+        totalTrips: { $sum: 1 },
+
+        totalEcoScore: { $sum: "totalScore" },
+        totalSpeedEcoScore: { $sum: "speedScore" },
+        totalRpmEcoScore: { $sum: "rpmScore" },
+        totalFeedbackConsiderationEcoScore: {
+          $sum: "feedbackConsiderationScore",
+        },
+        //totalKmPerTrip: { $max: "odometer" },
+        totalTripMinutesDuration: {
+          $sum: { $subtract: ["endTimestamp", "startTimestamp"] },
+        },
+
+        maxEcoScorePerTrip: { $max: "totalScore" },
+        maxSpeedEcoScorePerTrip: { $max: "speedScore" },
+        maxRpmEcoScorePerTrip: { $max: "rpmScore" },
+        maxFeedbackConsiderationEcoScorePerTrip: {
+          $max: "feedbackConsiderationScore",
+        },
+        /*maxKmPerTrip: { $max: "odometer" },*/
+        maxTripMinutesDuration: {
+          $max: {
+            $sum: { $subtract: ["endTimestamp", "startTimestamp"] },
+          },
+        },
       },
     },
-    {
-      $sort: { tripsPerDay: -1 },
-    },
   ]);
+};
 
-  return tripsPerDay ? tripsPerDay[0] : 0;
+const performancesInADay = async (userId) => {
+  performancesInInterval(
+    { userId: userId },
+    { $dateToString: { format: "%Y-%m-%d", date: "$at" } }
+  );
+};
+
+const performancesInAMonth = async (userId) => {
+  performancesInInterval(
+    { userId: userId },
+    { $dateToString: { format: "%Y-%m-%d", date: "$at" } }
+  );
+};
+
+//milestone
+const performancesSinceJoined = (userId) => {
+  performancesInInterval({ userId: userId });
+};
+
+//ENTRY POINT (this could be invoked by an independent route too)
+const assignAchievements = async (userId) => {
+  achievementsEvents = []; //array of strings
+
+  //============= eco-friendly behaviour ======================
+  electricTrips(userId)
+    .then((electricTripsCount) => {
+      if (electricTripsCount > 10) {
+        achievementsEvents.push("electric_10");
+      } else if (electricTripsCount > 5) {
+        achievementsEvents.push("electric_5");
+      } else if (electricTripsCount > 1) {
+        achievementsEvents.push("electric_1");
+      }
+    })
+    .catch((err) => console.log("error during electricTrips"));
+
+  const performances = await performancesInAMonth(userId);
+  const maxEcoScorePerTrip = Math.max(
+    ...performances.map((o) => o.maxEcoScorePerTrip)
+  );
+  if (maxEcoScorePerTrip > 10) {
+    achievementsEvents.push("electric_10");
+  } else if (maxEcoScorePerTrip > 5) {
+    achievementsEvents.push("electric_5");
+  } else if (maxEcoScorePerTrip > 1) {
+    achievementsEvents.push("electric_1");
+  }
+  const totalEcoScore = Math.max(...performances.map((o) => o.totalEcoScore));
+  if (totalEcoScore > 10) {
+    achievementsEvents.push("electric_10");
+  } else if (totalEcoScore > 5) {
+    achievementsEvents.push("electric_5");
+  } else if (totalEcoScore > 1) {
+    achievementsEvents.push("electric_1");
+  }
+  const totalSpeedEcoScore = Math.max(
+    ...performances.map((o) => o.totalSpeedEcoScore)
+  );
+  if (totalSpeedEcoScore > 10) {
+    achievementsEvents.push("electric_10");
+  } else if (totalSpeedEcoScore > 5) {
+    achievementsEvents.push("electric_5");
+  } else if (totalSpeedEcoScore > 1) {
+    achievementsEvents.push("electric_1");
+  }
+
+  //============= app usage ======================
+  const totalTrips = Math.max(...performances.map((o) => o.totalTrips));
+  if (totalTrips > 10) {
+    achievementsEvents.push("electric_10");
+  } else if (totalTrips > 5) {
+    achievementsEvents.push("electric_5");
+  } else if (trtotalTripsips > 1) {
+    achievementsEvents.push("electric_1");
+  }
+
+  //publish dell'evento... allo user service: client, topic, payload (to move to controller)
+  publish(userId, achievementsEvents, {}); //assign badges to users micro
 };
 
 //============encouraging eco-driving=================
@@ -142,128 +214,10 @@ const electricTrips = async (userId) => {
   // }
 };
 
-const fetchFuelType = async (vin) => {
-  userVehiclesUrl = "http://vehicles:8087/api/v1/userVehicles/" + vin; //read from config files
-  const response = await axios.get(userVehiclesUrl, {});
-  vehiclesModelUrl =
-    "http://vehicles:8087/api/v1/vehiclesModels/vehicleDetails/" +
-    response.data.vehicleId;
-  const vehicleDetails = await axios.get(vehiclesModelUrl, {});
-  return vehicleDetails.fuelType + vehicleDetails.fuelType1;
-};
-
-/*
-const gplDrives = (trip) => {
-  //trip.measurements.filter((date) => date.toDateString()).length;
-};*/
-
-//all the scores for performance reason
-const scoresGatheredInAMonth = (trip) => {};
-
-const scoresGatheredInADay = (trip) => {};
-
-//milestone
-const scoresGatheredSinceJoined = (trip) => {};
-
-const personalBestsSinceJoined = (userId) => {
-  //parziali, totale, min, max di km, score, minuti,
-  const tripsPerDay = Trip.aggregate([
-    //{ $match: { userId: userId } }, //filter only data of requested trip
-    {
-      $group: {
-        _id: { userId: userId },
-        totalTrips: { $sum: 1 },
-
-        totalEcoScore: { $sum: "totalScore" },
-        totalSpeedEcoScore: { $sum: "speedScore" },
-        totalRpmEcoScore: { $sum: 1 },
-        totalFeedbackConsiderationEcoScore: { $sum: 1 },
-
-        maxEcoScorePerTrip: { $max: 1 },
-        maxSpeedEcoScorePerTrip: { $max: 1 },
-        maxRpmEcoScorePerTrip: { $max: 1 },
-        maxFeedbackConsiderationEcoScorePerTrip: { $max: 1 },
-        maxKmPerTrip: { $sum: 1 },
-        maxTripMinutesDuration: { $sum: 1 },
-      },
-    },
-    {
-      $sort: { tripsPerDay: -1 },
-    },
-  ]);
-};
-
-//this could be invoked by an independent route too
-const assignAchievements = async (userId) => {
-  achievementsEvents = []; //array of strings
-
-  const achievementsEvents = getAchievements();
-  //publish dell'evento... allo user service
-  bus.publish(userId, achievementsEvents); //assign badges to user micro  //or by rest //USE MQTT CLIENT HERE
-};
+//
+//const gplDrives = (trip) => {
+//};
 
 module.exports = {
   assignAchievements,
 };
-
-//const userRankingFrom = (userId, fromDate) => {};
-
-/*const checkUseDaysInARow = (trip) => {
-  //segnalo l'achievement più alto (acheivements non cumulativi: quello più alto non determina quello/i più basso/i)
-  //uso il trip tanto ce l'ho in memoria... al limite posso ri-fetching it" qui
-  groupBy(trip.measurements, (date) => date.getYear());
-};
-
-const checkUseDays = (trip) => {
-  groupBy(trip.measurements, (date) => date.toDateString()).length;
-};*/
-
-/*
-var groupBy = function (xs, key) {
-  return xs.reduce(function (rv, x) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, {});
-};*/
-
-//from https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
-function getWeekNumber(d) {
-  // Copy date so don't modify original
-  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-  // Set to nearest Thursday: current date + 4 - current day number
-  // Make Sunday's day number 7
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-  // Get first day of year
-  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  // Calculate full weeks to nearest Thursday
-  var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-  // Return array of year and week number
-  return [d.getUTCFullYear(), weekNo];
-}
-
-var result = getWeekNumber(new Date());
-
-function consecutive(array) {
-  var i = 2,
-    d;
-  while (i < array.length) {
-    d = array[i - 1] - array[i - 2];
-    if (Math.abs(d) === 1 && d === array[i] - array[i - 1]) {
-      return false;
-    }
-    i++;
-  }
-  return true;
-}
-
-function groupByAndSum() {
-  var result = [];
-  array.reduce(function (res, value) {
-    if (!res[value.Id]) {
-      res[value.Id] = { Id: value.Id, qty: 0 };
-      result.push(res[value.Id]);
-    }
-    res[value.Id].qty += value.qty;
-    return res;
-  }, {});
-}
