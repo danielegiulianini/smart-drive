24197f299a676e7ac00ab127cac168a3435d5068
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-10-09 15:41:24 +0200

    implement rpm and idling feedbacks

--- backend/trips/src/services/trips.drivingAssistant.js
+++ backend/trips/src/services/trips.drivingAssistant.js
@@ -1,4 +1,6 @@
+const trips = require("../models/trips");
 const Trip = require("../models/trips");
+const TripStatsService = require("../services/trips.stats");
 
 //this writes to db
 const getAndAssignFeedback = async (tripId, measurementPayload) => {
@@ -6,16 +8,29 @@ const getAndAssignFeedback = async (tripId, measurementPayload) => {
 
   if (feedbacks) {
     let trip = await Trip.findById(tripId);
-    trip.feedbacks.push(feedbacks[0]); //ritorno quello con più prorità più alta
+    trip.feedbacks.push(sortByProperty(await feedbacks, "priority")); //ritorno quello con più prorità più alta
     await trip.save();
   }
-  return feedbacks[0];
+  return feedbacks ? feedbacks[0] : null;
 };
 
-//feedbacks are given analyzing not scores but only exact measurements
-//se velocità è sopra ad un limite e ho già dato tempo di adattarsi al feedback... allora
+const sortByProperty = (array, propertyName) => {
+  array.sort((a, b) =>
+    a[propertyName] > b[propertyName]
+      ? 1
+      : b[propertyName] > a[propertyName]
+      ? -1
+      : 0
+  );
+};
+
+const millis_in_seconds = 1000;
+
+//feedbacks are given analyzing either scores or exact measurements
 const getFeedbacks = async (tripId, measurementPayload) => {
   let feedbacks = [];
+
+  const trip = await Trip.findById(tripId);
 
   /* measurement:
      timestamp: { type: Date },
@@ -39,15 +54,51 @@ const getFeedbacks = async (tripId, measurementPayload) => {
       },
       */
 
-  //oppure se la media delle velocità è > X e l'ultimo consiglio risale a X fa ...
-  //rpm
+  //se velocità è sopra ad un limite e ho già dato tempo di adattarsi al feedback... allora
+  //oppure se la media delle velocità è > X e l'ultimo consiglio risale da X secs fa ...
+  //1. =========================== rpm ========================================
+  const rpmWindowInSeconds = 10;
+  if (
+    TripStatsService.computeOtherStats(tripId, new Date() - rpmWindowInSeconds)
+      .avgRpm > 3000
+  ) {
+    feedbacks.push({ text: "Be smoother with the throttle.", priority: 2 });
+  }
+  /*
+  or based on single measurement: 
   if (measurementPayload.rpm > 10) {
     feedbacks.push({ text: "", priority: 2 });
   } else if (measurementPayload.rpm > 5) {
     //speed
+  }*/
+
+  //3. =========================== reduce unnecessary emissions (idling) ===============
+  //(spegni il motore se è da più di 10 secondi che sei fermo (v=0))
+
+  const idleWindowInSeconds = 10;
+  if (
+    TripStatsService.computeOtherStats(
+      tripId,
+      new Date() - idleWindowInSeconds * millis_in_seconds
+    ).avgKph == 0
+  ) {
+    feedbacks.push({
+      text: "Turn the engine off to avoid unnecessary emissions.",
+      priority: 1,
+    });
   }
 
-  //spegni il motore se è da più di 4 secondi che sei fermo (v=0)
+  //2. =========================== speed limits ===========================
+  //compare with speedlimits
+
+  //5. =========================== not exceeding driving time without rest =============
+  //if there is not a break (no data for periods as car turn off) or there is no period with v == 0 in last x minutes => feedback
+
+  //4. =========================== keep safe following distance ========================
+  //PIR needed
+
+  //6. =========================== avoid smartphone distraction =======================
+  //count smartphone touches...
 
   return feedbacks;
 };
