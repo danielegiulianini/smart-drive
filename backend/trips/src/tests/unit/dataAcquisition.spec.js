325c51f938d6a54f4b8b21b4b023474bf46ad7d3
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-10-09 22:20:40 +0200

    fix timestamp setting for new measurements

--- backend/trips/src/tests/unit/dataAcquisition.spec.js
+++ backend/trips/src/tests/unit/dataAcquisition.spec.js
@@ -66,8 +66,8 @@ describe("a trip", () => {
     it("should set the end timestamp accordingly", async () => {
       let savedTrip = await TripsService.addTrip(fakeTripData);
       await TripsService.close(savedTrip._id);
-      let savedTrip2 = await TripsService.get(savedTrip._id);
-      validateNotEmpty(savedTrip2.endTimestamp);
+      let fetchedTrip = await TripsService.get(savedTrip._id);
+      validateNotEmpty(fetchedTrip.endTimestamp);
     });
 
     it("should trigger an error if never added", async () => {
@@ -127,7 +127,7 @@ const fakeMeasurementData = {
 };
 
 describe("a measurement", () => {
-  //aggiungo un measurement e me lo ritrovo  con i dati che ho inserito
+  //aggiungo un measurement e me lo ritrovo con i dati che ho inserito
   describe("when added", () => {
     it("should get its timestamp set accordingly", async () => {
       let savedTrip = await TripsService.addTrip(fakeTripData);
@@ -136,13 +136,17 @@ describe("a measurement", () => {
 
       const fetchedTrip = await TripsService.get(savedTrip._id);
 
-      /*
-      FROM: https://medium.com/@andrei.pfeiffer/jest-matching-objects-in-array-50fe2f4d6b98 
-      const state = [
-  { type: 'START', data: 'foo' },
-  { type: 'START', data: 'baz' },
-  { type: 'END', data: 'foo' },
-]*/
+      validateNotEmpty(fetchedTrip.measurements[0].timestamp);
+    });
+
+    it("should get its values persisted correctly", async () => {
+      let savedTrip = await TripsService.addTrip(fakeTripData);
+
+      await TripsService.addMeasurement(fakeVin, fakeMeasurementData); //add measurement should attach measurements to the (unique) trip with that fakeVin
+
+      const fetchedTrip = await TripsService.get(savedTrip._id);
+
+      //FROM: https://medium.com/@andrei.pfeiffer/jest-matching-objects-in-array-50fe2f4d6b98
       expect(fetchedTrip.measurements).toEqual(
         // 1
         expect.arrayContaining([
@@ -154,95 +158,15 @@ describe("a measurement", () => {
             odometer: parseInt(fakeOdometer),
           }),
         ])
-      );
 
-      /*
+        /*
        you expect that:
 1.  your Array equals
 2. an Array that contains
 3. an Object that contains
 4. the following properties
-
 */
-      /*DA RIVEDERE: expect(savedTrip.measurements).toContain(fakeMeasurementData);
-      validateNotEmpty(savedMeasurement.timestamp);*/
+      );
     });
-
-    it("should get its values persisted correctly", async () => {});
   });
 });
-
-/*
-let mongo = null;
-const connectDB = async () => {
-  mongo = await MongoMemoryServer.create();
-  const uri = mongo.getUri();
-
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};*/
-
-/*
-const User = require('../user.model');
-const { fakeUserData } = require('../../fixtures');
-const {
-  validateNotEmpty,
-  validateStringEquality,
-  validateMongoDuplicationError,
-} = require('../../../utils/test-utils/validators.utils');
-const {
-  dbConnect,
-  dbDisconnect,
-} = require('../../../utils/test-utils/dbHandler.utils');
-
-beforeAll(async () => dbConnect());
-afterAll(async () => dbDisconnect());
-
-describe('User Model Test Suite', () => {
-  test('should validate saving a new student user successfully', async () => {
-    const validStudentUser = new User({
-      local: fakeUserData,
-      role: fakeUserData.role,
-    });
-    const savedStudentUser = await validStudentUser.save();
-
-    validateNotEmpty(savedStudentUser);
-
-    validateStringEquality(savedStudentUser.role, fakeUserData.role);
-    validateStringEquality(savedStudentUser.local.email, fakeUserData.email);
-    validateStringEquality(
-      savedStudentUser.local.username,
-      fakeUserData.username
-    );
-    validateStringEquality(
-      savedStudentUser.local.password,
-      fakeUserData.password
-    );
-    validateStringEquality(
-      savedStudentUser.local.firstName,
-      fakeUserData.firstName
-    );
-    validateStringEquality(
-      savedStudentUser.local.lastName,
-      fakeUserData.lastName
-    );
-  });
-
-  test('should validate MongoError duplicate error with code 11000', async () => {
-    expect.assertions(4);
-    const validStudentUser = new User({
-      local: fakeUserData,
-      role: fakeUserData.role,
-    });
-
-    try {
-      await validStudentUser.save();
-    } catch (error) {
-      const { name, code } = error;
-      validateMongoDuplicationError(name, code);
-    }
-  });
-});
-*/
