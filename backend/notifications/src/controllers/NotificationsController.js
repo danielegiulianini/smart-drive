37273d917781cc6a8cd8fa12a77612138c563428
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-11-01 09:29:03 +0100

    add socket.io support for notifications

--- backend/notifications/src/controllers/NotificationsController.js
+++ backend/notifications/src/controllers/NotificationsController.js
@@ -1,22 +1,44 @@
-const isMessageValid = (message) =>
-  message.target && message.subject && message.textBody && message.htmlBody;
+function logout(context) {
+  onUserDisconnected(context);
+  context.userId = undefined;
+}
 
-async function onMessageArrived(msg) {
-  const message = JSON.parse(msg.toString());
-
-  console.log("Received '%s'", message);
-
-  // Check if the message packet has the required fields
-  if (isMessageValid(message)) {
-    console.log("message is valid!");
-    // Call the send function on EmailService (in my case: import the API for sending over socket.io)
-    /*await emailService
-    .send(message.target, message.subject, message.textBody, message.htmlBody)
-    .then((p) => console.log(`[AMQP] Done, message ID : ${p.messageId}`))
-    .catch((err) => console.log(`[ERROR] Error: ${err}`));*/
-  } else {
-    console.log(`[WARNING] Message did not respect standard format`);
+function onUserDisconnected(context) {
+  if (context.userId) {
+    console.log(
+      `User with id ${context.userId} disconnected from notifications`
+    );
   }
 }
 
-module.exports = { onMessageArrived };
+//used when sending notification
+function onNewNotification(context, notification) {
+  if (context.userId) {
+    context.socket.emit("notification", notification);
+  } else {
+    console.log(
+      `User with id ${context.userId} has never been connected to notifications microservice`
+    );
+  }
+}
+
+function onNewDrivingNotification(context, notification) {
+  if (context.userId) {
+    context.socket.volatile.emit("notification", notification);
+  } else {
+    console.log(
+      `User with id ${context.userId} has never been connected to notifications microservice`
+    );
+  }
+}
+
+//used by index.html
+exports.onConnection = function (socket) {
+  let context = {
+    socket,
+  };
+  socket.on("logout", () => logout(context));
+  socket.on("disconnect", function () {
+    onUserDisconnected(context);
+  });
+};
