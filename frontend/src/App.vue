8527731e753440b46a7cb53ef1cbf84a2cdb5b32
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-11-11 11:50:09 +0100

    adapt App.vue to use router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,7 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!--<HelloWorld msg="Vite + Vue" />-->
+  <router-view></router-view>
 </template>
 
 <style scoped>
