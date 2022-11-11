cd9b69d250252374b89a897391dd97f7bf390e72
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-11-11 16:20:35 +0100

    add route for trips at frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HelloWorld from '/src/components/HelloWorld.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HelloWorld from "../components/HelloWorld.vue";
+import TheAppProfilePage from "../views/TheAppProfilePage.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: HelloWorld,
-    },
-]
+  {
+    path: "/",
+    name: "Home",
+    component: HelloWorld,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    //props:true,
+    component: TheAppProfilePage,
+  },
+  {
+    path: "/trips",
+    name: "Trips",
+    //props:true,
+    component: TripsOverviewPage,
+  }
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
