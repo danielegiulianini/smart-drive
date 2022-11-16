777429e1d8a7f9ba9d7a3dcf2f97a20b187914bf
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-11-16 17:36:32 +0100

    add search bar frontend component

---
+++ frontend/src/components/AppCenteredSearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- CENTERED SEARCH BAR -->
+  <div
+    class="form-group d-flex has-search m-2 text-center mx-auto"
+    style="max-width: 300px; width: 100%"
+  >
+    <input type="text" class="form-control" placeholder="Search" />
+    <button
+      type="submit"
+      title="Search"
+      style="border: 0; padding: 0; margin-left: -30px; background: none"
+    >
+      <i class="bi bi-search"></i>
+      <!-- or  <span class="fa fa-search form-control-feedback"></span>-->
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
