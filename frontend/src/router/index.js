9c439a309e0457515f259f72d0b466a2270cf442
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-11-19 16:55:34 +0100

    add frontend route for score page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,9 +3,14 @@ import HelloWorld from "../components/HelloWorld.vue";
 import TheAppProfilePage from "../views/TheAppProfilePage.vue";
 import TripsOverviewPage from "../views/TripsOverviewPage.vue";
 import TripDetailPage from "../views/TripDetailPage.vue";
+import TripDetailPage2 from "../views/TripDetailPage2.vue";
+
 import DashboardPage from "../views/DashboardPage.vue";
 import GaragePage from "../views/GaragePage.vue";
 import VehicleDetailPage from "../views/VehicleDetailPage.vue";
+import VehicleDetailPage2 from "../views/VehicleDetailPage2.vue";
+import ScorePage from "../views/ScorePage.vue";
+
 const routes = [
   {
     path: "/",
@@ -25,9 +30,12 @@ const routes = [
     component: TripsOverviewPage,
   },
   { path: "/trips/:id", component: TripDetailPage },
+  { path: "/trips2/:id", component: TripDetailPage2 },
   { path: "/dashboard", component: DashboardPage },
   { path: "/garage", component: GaragePage }, //or vehicles...
   { path: "/vehicle/:id", component: VehicleDetailPage }, //or vehicles...
+  { path: "/vehicle2/:id", component: VehicleDetailPage2 }, //or vehicles...
+  { path: "/scores/:id", component: ScorePage }, //or vehicles...
 ];
 
 const router = createRouter({
