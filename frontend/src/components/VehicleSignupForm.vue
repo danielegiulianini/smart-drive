5e584deff7159e7f91cba74e9b033970c8b2a71f
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-11-23 22:50:17 +0100

    add skeleton for vehicle registration

---
+++ frontend/src/components/VehicleSignupForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <!--year -->
+    <select name="cars" id="year">
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="opel">Opel</option>
+      <option value="audi">Audi</option>
+    </select>
+    <!--make -->
+    <select name="cars" id="make">
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="opel">Opel</option>
+      <option value="audi">Audi</option>
+    </select>
+    <!--model -->
+    <select name="cars" id="model">
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="opel">Opel</option>
+      <option value="audi">Audi</option>
+    </select>
+    <!--series -->
+    <select name="cars" id="series">
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="opel">Opel</option>
+      <option value="audi">Audi</option>
+    </select>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    submitForm: function (e) {
+      e.prevent;
+    },
+  },
+};
+</script>
