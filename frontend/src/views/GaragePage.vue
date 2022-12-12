46a7c8d661edcf5f13d55e5de16f26175e26fc59
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-12 23:18:35 +0100

    fix fetching of vehicles details

--- frontend/src/views/GaragePage.vue
+++ frontend/src/views/GaragePage.vue
@@ -28,15 +28,17 @@
     <div class="card p-1 mb-3">
       <div class="d-flex justify-content-between m-2">
         <div class="d-flex my-auto">
-          <span><i class="bi bi-speedometer2"></i> 3 vehicles</span>
+          <span
+            ><i class="bi bi-speedometer2"></i>
+            {{ userVehicles.length }} vehicles</span
+          >
           <small class="text-muted"> | </small>
           <span><i class="bi bi-car-front"></i> 3 km </span>
           <small class="text-muted"> | </small>
           <span><i class="bi bi-clock"></i>1h22 mins</span>
-
-          <!--<div>Total: 3 vehicles</div>-->
         </div>
         <div>
+          <!-- desktop add vehicle button-->
           <button
             class="btn btn-light d-none d-sm-block"
             type="button"
@@ -81,19 +83,17 @@
                   <!-- :key is a low-level hint for Vue -->
                   <!-- _id is the _id of userVehicle (not of vehicleModel)-->
                   <VehicleCard
-                    v-for="(vehicle, index) in userVehiclesWithModelDetails"
+                    v-for="(vehicle, index) in activeUserVehicles"
                     :key="vehicle._id"
                     :_id="vehicle._id"
                     :make="vehicle.make"
                     :model="vehicle.model"
                     :makeLogoImgUrl="makeLogoImgUrlByMake[vehicle.make]"
+                    :year="vehicle.year"
                     :comb08="vehicle.comb08"
                     :fuelType="vehicle.fuelType"
                     :trany="vehicle.trany"
                   ></VehicleCard>
-                  <VehicleCard></VehicleCard>
-                  <VehicleCard></VehicleCard>
-                  <VehicleCard></VehicleCard>
                 </div>
               </div>
             </div>
@@ -108,7 +108,7 @@
                 aria-expanded="false"
                 aria-controls="flush-collapseTwo"
               >
-                Ritirati
+                Retired
               </button>
             </h2>
             <div
@@ -121,8 +121,18 @@
                 <div
                   class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-5 g-4"
                 >
-                  <VehicleCard></VehicleCard>
-                  <VehicleCard></VehicleCard>
+                  <VehicleCard
+                    v-for="(vehicle, index) in ritiredUserVehicles"
+                    :key="vehicle._id"
+                    :_id="vehicle._id"
+                    :make="vehicle.make"
+                    :model="vehicle.model"
+                    :makeLogoImgUrl="makeLogoImgUrlByMake[vehicle.make]"
+                    :year="vehicle.year"
+                    :comb08="vehicle.comb08"
+                    :fuelType="vehicle.fuelType"
+                    :trany="vehicle.trany"
+                  ></VehicleCard>
                 </div>
               </div>
             </div>
@@ -143,8 +153,9 @@ import TheAppFooter from "../components/TheAppFooter.vue";
 import TheAppMobileNavbar from "../components/TheAppMobileNavbar.vue";
 import VehicleCard from "../components/VehicleCard.vue";
 import MobileAddVehicleButton from "../components/MobileAddVehicleButton.vue";
-import makesLogosData from "../assets/data.json"; //vehicle-make's logos
 import axios from "axios";
+
+import makesLogosData from "../assets/data.json"; //vehicle-make's logos
 
 export default {
   components: {
@@ -157,9 +168,19 @@ export default {
   },
   data() {
     return {
-      userVehicles: [],
-      //not reactive:
-      makeLogoImgUrlByMake: makesLogosData //a data structure optimized for access
+      userVehicles: [
+        {
+          _id: "ciao",
+          make: "Fiat",
+          model: "panda",
+          makeLogoImgUrl: "",
+          comb08: "89",
+          fuelType: "diesel",
+          trany: "Automatic",
+        },
+      ],
+      //not reactive data:
+      makeLogoImgUrlByMake: makesLogosData //reducing creates a data structure optimized for access (more efficient than makesLogosData)
         .reduce(function (result, item, index, array) {
           result[item.name] = item.image.source;
           return result;
@@ -167,49 +188,96 @@ export default {
     };
   },
   computed: {
-    /* activeUserVehicles: userVehicles.filter((vehicle) => !vehicle.retired),
+    /* this works? :
+    activeUserVehicles: userVehicles.filter((vehicle) => !vehicle.retired),
     ritiredUserVehicles: userVehicles.filter((vehicle) => vehicle.retired),
     vehiclesCount: userVehicles.length,*/
     activeUserVehicles() {
-      return userVehicles.filter((vehicle) => !vehicle.retired);
+      return this.userVehicles.filter((vehicle) => !vehicle.retired);
     },
     ritiredUserVehicles() {
-      return userVehicles.filter((vehicle) => vehicle.retired);
+      return this.userVehicles.filter((vehicle) => vehicle.retired);
     },
     activeUserVehiclesCount() {
-      return userVehicles().length;
+      return this.userVehicles().length;
     },
     ritiredUserVehiclesCount() {
-      return ritiredUserVehicles().length;
+      return this.ritiredUserVehicles().length;
     },
   },
-  methods: {},
+  methods: {
+    onNewVehicleSubmit(event) {
+      //maybe to move in separate form/modal component
+      //-----client-side validation (could also be made on input event)-----------
+      //cannot add same vin twice
+      //--------------------------------------
+      //-----server-side validation-----------
+      //--------------------------------------
+      const loggedInUserId = 12; //this.$store.state.user.id; TODO
+      axios
+        .get(`vehicles/userVehicles?userId=${loggedInUserId}`)
+        .then((result) => {
+          const userVehicles = result.data;
+          const promises = userVehicles.map((userVehicle) => {
+            return axios
+              .get(
+                `vehicles/vehiclesModels/vehicleDetails/${userVehicle.vehicleModelId}`
+              ) //retrieving vehicles details
+              .then((res) => {
+                res.data;
+              });
+          });
+          /*Promise.all(promises)
+            .then((usersVehicleWithModelDetails) => {
+              console.log(usersVehicleWithModelDetails);
+              this.userVehicles = usersVehicleWithModelDetails;
+            })
+            .catch((err) => {
+              throw err; //rethrowing a error
+            });*/
+        })
+        .catch((err) => console.error(err)); //communicate something to user?
+    },
+  },
   mounted() {
     console.log("makes logo data is: ", this.makeLogoImgUrlByMake);
+    console.log("l'url della fiat is:", this.makeLogoImgUrlByMake["Fiat"]);
     //fetch all the vehicles of the user from vehicles microservice and pass as props to vehicle cards!
     //manually joining userVehicles and vehicleModels here
-    /*const loggedInUserId = 12; //this.$store.state.user.id;
+    const loggedInUserId = 12; //this.$store.state.user.id;
     axios
       .get(`vehicles/userVehicles?userId=${loggedInUserId}`)
       .then((result) => {
+        //fetching (in parallel) all uservehicles details
         const userVehicles = result.data;
-        const promises = userVehicles.map((userVehicle) => {
-          return axios
-            .get(`vehicles/vehicleModels/vehicleDetails/${userVehicle.vehicleModelId}`)//retrieving vehicles details
-            .then((res) => {
-              res.data;
-            });
-        });
+        console.log("cio che mi riorna il backend is: ", result.data);
+        const promises = userVehicles.map((userVehicle) =>
+          axios
+            .get(
+              `vehicles/vehiclesModels/vehicleDetails/${userVehicle.vehicleModelId}`
+            ) //retrieving vehicles details
+            .then((res) => res.data)
+            .catch((error) => console.log(error))
+        );
         Promise.all(promises)
-          .then((usersVehicleWithModelDetails) => {
-            console.log(usersVehicleWithModelDetails);
-            this.userVehicles = usersVehicleWithModelDetails;
+          //assigning make url
+          .then((usersVehiclesWithModelDetails) =>
+            usersVehiclesWithModelDetails.map(
+              (userVehicle) =>
+                (userVehicle.makeLogoImgUrl =
+                  this.makeLogoImgUrlByMake[userVehicle.make])
+            )
+          )
+          //assigning to data
+          .then((usersVehiclesWithModelDetails) => {
+            console.log(usersVehiclesWithModelDetails);
+            this.userVehicles = usersVehiclesWithModelDetails;
           })
           .catch((err) => {
-            throw err; //rethrowing a error
+            throw err; //re-throwing a error
           });
       })
-      .catch((err) => console.error(err)); //communicate something to user?*/
+      .catch((err) => console.error(err)); //communicate something to user (with a mre user-friendly mapping?)*/
   },
 };
 </script>
