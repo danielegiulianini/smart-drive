ce3da3de01ecf1b5cce638d1962bc361e5cc2faa
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-17 23:10:09 +0100

    complete vehicle statistics tab pane fetching from fueleconomy

--- frontend/src/components/VehicleStatisticsTabPane.vue
+++ frontend/src/components/VehicleStatisticsTabPane.vue
@@ -1,184 +1,254 @@
 <template>
   <div
-    class="tab-pane fade"
+    class="tab-pane"
+    :class="{ active: isActive }"
     id="bordered-justified-profile"
     role="tabpanel"
     aria-labelledby="profile-tab"
   >
     <!-- START OF CARD WRAPPed by tab-pane-->
     <div class="card p-2 m-2" style="box-shadow: none">
-      <div class="row row-cols-1 g-3">
-        <div class="col">
-          <div class="card nested-card">
-            <div class="card-body">
-              <h5 class="card-title">Line Chart</h5>
+      <div class="card-body">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseOne"
+                aria-expanded="true"
+                aria-controls="flush-collapseOne"
+              >
+                <h1 style="font-size: 120%; font-family: Poppins, sans-serif">
+                  Scores
+                </h1>
+              </button>
+            </h2>
+            <div
+              id="flush-collapseOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="flush-headingOne"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                <!-- could have used a v-for if using array instead of object for vehicle's properties-->
+                <ul class="list-group list-group-flush">
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="max total score"
+                      :value="statistics.totalScore.max"
+                    ></VehicleDetailItem>
+
+                    <VehicleDetailItem
+                      :right="true"
+                      label="avg total score"
+                      :value="statistics.totalScore.avg"
+                    ></VehicleDetailItem>
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="min total score"
+                      :value="statistics.totalScore.min"
+                    ></VehicleDetailItem>
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="max safety score"
+                      :value="statistics.speedScore.max"
+                    ></VehicleDetailItem>
+
+                    <VehicleDetailItem
+                      :right="true"
+                      label="avg safety score"
+                      :value="statistics.speedScore.avg"
+                    ></VehicleDetailItem>
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="min safety score"
+                      :value="statistics.speedScore.min"
+                    ></VehicleDetailItem>
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="max aggressiveness score"
+                      :value="statistics.rpmScore.max"
+                    ></VehicleDetailItem>
+
+                    <VehicleDetailItem
+                      :right="true"
+                      label="avg aggressiveness score"
+                      :value="statistics.rpmScore.avg"
+                    ></VehicleDetailItem>
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="min aggressiveness score"
+                      :value="statistics.rpmScore.min"
+                    ></VehicleDetailItem>
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="max feedback score"
+                      :value="statistics.feedbackConsiderationScore.max"
+                    ></VehicleDetailItem>
+
+                    <VehicleDetailItem
+                      :right="true"
+                      label="avg feedback score"
+                      :value="statistics.feedbackConsiderationScore.avg"
+                    ></VehicleDetailItem>
+                  </li>
+                  <li
+                    class="list-group-item d-flex justify-content-between align-middle"
+                  >
+                    <VehicleDetailItem
+                      label="min feedback score"
+                      :value="statistics.feedbackConsiderationScore.min"
+                    ></VehicleDetailItem>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingTwo">
+              <button
+                class="accordion-button collapsed fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseTwo"
+                aria-expanded="false"
+                aria-controls="flush-collapseTwo"
+              >
+                <div style="font-size: 120%" class="fw-bold">Trip Distance</div>
+              </button>
+            </h2>
+            <div
+              id="flush-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="flush-headingTwo"
+              data-bs-parent="#accordionFlushExample2"
+            >
               <ul class="list-group list-group-flush">
                 <li
                   class="list-group-item d-flex justify-content-between align-middle"
                 >
-                  <div>
-                    <span
-                      class="card-text text-muted"
-                      style="line-height: 50%; font-size: 90%"
-                    >
-                      year
-                    </span>
-                    <span class="card-text text-bf" style="font-size: 110%"
-                      >2020</span
-                    >
-                  </div>
+                  <VehicleDetailItem
+                    label="max total score"
+                    :value="statistics.tripDistance.max"
+                  ></VehicleDetailItem>
 
-                  <div>
-                    <span class="card-text text-bf" style="font-size: 105%"
-                      >Automatic</span
-                    >
-                    <span
-                      class="card-text text-muted"
-                      style="line-height: 50%; font-size: 90%"
-                    >
-                      trany
-                    </span>
-                  </div>
+                  <VehicleDetailItem
+                    :right="true"
+                    label="avg total score"
+                    :value="statistics.tripDistance.avg"
+                  ></VehicleDetailItem>
                 </li>
-                <li class="list-group-item">
-                  <div class="d-flex justify-content-between align-middle">
-                    <div>
-                      <span
-                        class="card-text text-muted"
-                        style="line-height: 50%; font-size: 90%"
-                      >
-                        year
-                      </span>
-                      <span class="card-text text-bf" style="font-size: 110%"
-                        >2020</span
-                      >
-                    </div>
+                <li
+                  class="list-group-item d-flex justify-content-between align-middle"
+                >
+                  <VehicleDetailItem
+                    label="max total score"
+                    :value="statistics.tripDistance.max"
+                  ></VehicleDetailItem>
 
-                    <div>
-                      <span class="card-text text-bf" style="font-size: 105%"
-                        >Automatic</span
-                      >
-                      <span
-                        class="card-text text-muted"
-                        style="line-height: 50%; font-size: 90%"
-                      >
-                        trany
-                      </span>
-                    </div>
-                  </div>
+                  <VehicleDetailItem
+                    :right="true"
+                    label="avg total score"
+                    :value="statistics.tripDistance.avg"
+                  ></VehicleDetailItem>
                 </li>
               </ul>
-
-              <!-- End Line Chart -->
             </div>
           </div>
-        </div>
-        <div class="col">
-          <div class="card nested-card">
-            <div class="card-body">
-              <h5 class="card-title">Line Chart</h5>
-
-              <!-- Line Chart -->
-              <div
-                id="lineChart"
-                style="
-                  min-height: 400px;
-                  user-select: none;
-                  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-                "
-                class="echart"
-                _echarts_instance_="ec_1667726733869"
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button
+                class="accordion-button collapsed fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseThree"
+                aria-expanded="false"
+                aria-controls="flush-collapseThree"
               >
-                <div
-                  style="
-                    position: relative;
-                    width: 450px;
-                    height: 400px;
-                    padding: 0px;
-                    margin: 0px;
-                    border-width: 0px;
-                    cursor: default;
-                  "
-                >
-                  <canvas
-                    data-zr-dom-id="zr_0"
-                    width="562"
-                    height="500"
-                    style="
-                      position: absolute;
-                      left: 0px;
-                      top: 0px;
-                      width: 450px;
-                      height: 400px;
-                      user-select: none;
-                      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-                      padding: 0px;
-                      margin: 0px;
-                      border-width: 0px;
-                    "
-                  ></canvas>
+                <div style="font-size: 120%" class="fw-bold">
+                  Estimated trips costs
                 </div>
-              </div>
+              </button>
+            </h2>
+            <div
+              id="flush-collapseThree"
+              class="accordion-collapse collapse"
+              aria-labelledby="flush-headingThree"
+              data-bs-parent="#accordionFlushExample3"
+            >
+              <ul class="list-group list-group-flush">
+                <li
+                  class="list-group-item d-flex justify-content-between align-middle"
+                >
+                  <VehicleDetailItem
+                    label="max total score"
+                    :value="statistics.estimatedTripCost.max"
+                  ></VehicleDetailItem>
 
-              <!-- End Line Chart -->
+                  <VehicleDetailItem
+                    :right="true"
+                    label="avg total score"
+                    :value="statistics.estimatedTripCost.avg"
+                  ></VehicleDetailItem>
+                </li>
+                <li
+                  class="list-group-item d-flex justify-content-between align-middle"
+                >
+                  <VehicleDetailItem
+                    label="max total score"
+                    :value="statistics.estimatedTripCost.max"
+                  ></VehicleDetailItem>
+                </li>
+              </ul>
+              <!--end first col-->
             </div>
           </div>
         </div>
-        <!-- end of col with line chart-->
       </div>
-      <!-- end of row of cards with chart-->
     </div>
     <!-- card wrapping row-->
   </div>
 </template>
 
 <script>
-import axios from "axios";
-
-import TheAppHeader from "./TheAppHeader.vue";
-import TheAppSidebar from "../components/TheAppSidebar.vue";
-import TheAppFooter from "../components/TheAppFooter.vue";
+import VehicleDetailItem from "./VehicleDetailItem.vue";
 
 export default {
-  components: {
-    TheAppHeader,
-    TheAppSidebar,
-    TheAppFooter,
-  },
+  inheritAttrs: false,
+  components: { VehicleDetailItem },
   props: {
-    progressPercentage: {
-      type: Number, // Number from 0.0 to 1.0
+    isActive: {
+      type: Boolean, // Number from 0.0 to 1.0
       required: true,
     },
-  },
-  data() {
-    //this cannot be a field because every instance of this component must have its own state
-    return {
-      author: {
-        name: "John Doe",
-        books: [
-          "Vue 2 - Advanced Guide",
-          "Vue 3 - Basic Guide",
-          "Vue 4 - The Mystery",
-        ],
-      },
-    };
+    statistics: {
+      type: Object,
+      required: true,
+    },
   },
 };
 </script>
 
-<style>
-.btn-circle.btn-md {
-  width: 50px;
-  height: 50px;
-  padding: 7px 10px;
-  border-radius: 25px;
-  font-size: 10px;
-  text-align: center;
-}
-.page-header {
-  padding: 20px;
-  background-color: white; /* ghostwhite;*/
-  border-radius: 10px;
-}
-</style>
+<style scoped></style>
