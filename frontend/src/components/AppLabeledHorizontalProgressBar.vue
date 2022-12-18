0adad1c8d9bb91795fcc94654c1c4d947b23edda
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-18 16:59:19 +0100

    add frontend/src/components/AppLabeledHorizontalProgressBar

---
+++ frontend/src/components/AppLabeledHorizontalProgressBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="progress mx-5" style="height: 3px; width: 50px">
+    <div
+      class="progress-bar progress-bar-striped progress-bar-animated bg-info"
+      role="progressbar"
+      :aria-valuenow="progress"
+      aria-valuemin="0"
+      :aria-valuemax="max"
+      style="width: 85%"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["progress", "max"],
+};
+</script>
