8804de6ecd8b64953d111d10e2d774f0f0473412
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-18 17:24:40 +0100

    add tooltip component

---
+++ frontend/src/components/AppTooltip.vue
@@ -0,0 +1,22 @@
+<template>
+  <i
+    class="bi bi-question-circle ps-2"
+    data-bs-toggle="tooltip"
+    data-bs-placement="top"
+    data-html="true"
+    :title="content"
+    ref="info"
+  >
+  </i>
+</template>
+
+<script>
+import { Tooltip } from "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+export default {
+  props: ["content"],
+  mounted() {
+    new Tooltip(this.$refs.info);
+  },
+};
+</script>
