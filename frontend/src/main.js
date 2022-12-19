edf988b4d34279154269fb329f2a9c7d4900039f
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-19 10:58:23 +0100

    make socket.io client available as a singleton accessible by components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,30 @@
-import { createApp } from 'vue'
-import './style.css'
-import './assets/style/global.scss'
+import { createApp } from "vue";
 
-import App from './App.vue'
-import router from "./router/index"
+import axios from "axios";
+axios.create();
+axios.defaults.baseURL = "http://localhost:8082/api/v1/"; //using backend api gateway here
+//"http://localhost:8086/api/v1/"//for testing direct nteraction with microservice
 
-createApp(App).use(router).mount('#app') 
+import Vue from "vue";
+import App from "./App.vue";
+import { io } from "socket.io-client";
+
+import router from "./router/index";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./assets/style/scss/global.scss";
+
+Vue.prototype.$socket = io("http://localhost:8082/api/v1/notifications"); //io(process.env.APP_API_URL_BASE);
+
+createApp(App)
+  //declaring all the packages used by the Vue instance (that pull their components in scope) (aka asset registration)
+  //.use(VueApexCharts);
+
+  .use(router)
+  .use(store)
+  .mount("#app");
+//The .mount() method should always be called after all app configurations and asset
+//registrations are done. Also note that its return value, unlike the asset registration methods,
+//is the root component instance instead of the application instance.
