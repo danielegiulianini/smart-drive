b915f99da4ecf9194dd5c38af4a5062cf5829132
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-21 17:12:38 +0100

    add notifications routes

---
+++ backend/notifications/src/routes/notifications.js
@@ -0,0 +1,17 @@
+//only a getall with wuery params (pushing internall y8with mqtt)
+
+const express = require("express");
+
+const AuthMiddleware = require("./../middlewares/auth");
+const NotificationController = require("../controllers/notifications");
+
+const router = express.Router();
+
+
+//router.use(AuthMiddleware.extractUserIdFromTokenAndPutItToBody) //set up middleware for post too? yes, because with supabase you have set it already
+
+router.route("/").post(NotificationController.create);
+
+router.route("/:notificationId").post(NotificationController.edit);
+
+router.route("/").get(NotificationController.list);
