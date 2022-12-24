80c427e8c5edb223b7ef927f46bc79ecc6aee9de
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-24 14:27:17 +0100

    fix size of user login page

--- frontend/src/views/UserLoginPage.vue
+++ frontend/src/views/UserLoginPage.vue
@@ -1,30 +1,29 @@
 <template>
   <spinner :show1="true"></spinner>
-
+  <!-- <div class="container">
+    <section
+      class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4"
+    >
+      <div class="container">
+        <div class="row justify-content-center">
+          <div
+            class="col-lg-12 d-flex flex-column align-items-center justify-content-center"
+          >
+            <UserLoginForm></UserLoginForm>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>-->
   <UserLoginForm></UserLoginForm>
 </template>
 
 <script>
-import axios from "axios";
 import UserLoginForm from "../components/UserLoginForm.vue";
 import Spinner from "../components/Spinner.vue";
 
 export default {
-  components: { UserLoginForm, Spinner},
-
-  data() {
-    //this cannot be a field because every instance of this component must have its own state
-    return {
-      author: {
-        name: "John Doe",
-        books: [
-          "Vue 2 - Advanced Guide",
-          "Vue 3 - Basic Guide",
-          "Vue 4 - The Mystery",
-        ],
-      },
-    };
-  },
+  components: { UserLoginForm, Spinner },
 };
 </script>
 
