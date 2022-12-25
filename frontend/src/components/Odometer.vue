5d6ef60a16805c10d3c7959d6043e2f7dac2e065
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-25 23:04:43 +0100

    add odometer

---
+++ frontend/src/components/Odometer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div style="font-size: 200%; line-height: 90%" class="pt-4">10000.00</div>
+    <div class="text-muted text-center" style="font-size: 105%">km</div>
+    <!--&lt;Vue3Odometer :value=&quot;num&quot; /&gt;-->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    rpm: {
+      required: true,
+    },
+  },
+  mounted() {},
+};
+</script>
+
+<style scoped></style>
