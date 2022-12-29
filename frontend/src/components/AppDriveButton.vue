acff39de78aa64ca22662eafede4f544ac5cc18e
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-29 23:20:15 +0100

    add drive button component

---
+++ frontend/src/components/AppDriveButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <button
+    id="driveButton"
+    type="button"
+    class="acceso"
+    style="
+      font-size: 120%;
+      border-radius: 100%;
+      width: 100px;
+      height: 100px;
+      border-width: 2px;
+      z-index: 99 !important;
+      border: solid;
+    "
+  >
+    <div class="d-flex justify-content-center align-middle">
+      <span
+        class="mt-2 spinner-border spinner-border-sm"
+        role="status"
+        aria-hidden="true"
+        v-if="spin"
+      ></span>
+      <div class="fw-bold" style="margin-bottom: -8px">
+        {{ driveButtonName }}
+      </div>
+    </div>
+    <small style="font-size: 70%">drive</small>
+  </button>
+</template>
+
+<style scoped>
+#driveButton.nonacceso {
+  background-color: #efefef;
+  border-color: #012970;
+  color: #012970;
+}
+#driveButton.acceso {
+  background-color: #012970;
+  border-color: #efefef;
+  color: #efefef;
+}
+
+#driveButton.acceso:hover {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+#driveButton.nonacceso:hover {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+</style>
+
+<script>
+/*here the logic */
+
+export default {
+  props: {
+    driveButtonName: { required: true },
+    isStarting: { default: false },
+  },
+};
+</script>
