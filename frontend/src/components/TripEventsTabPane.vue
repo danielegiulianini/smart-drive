3646eac97dbd4da9135558f2184557b702ae9568
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-29 23:19:05 +0100

    add component for trips events

---
+++ frontend/src/components/TripEventsTabPane.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- QUI INIZIA IL secondo (graph) TAB pane-->
+  <div
+    class="tab-pane"
+    :class="{ active: isActive }"
+    id="bordered-justified-contact2"
+    role="tabpanel"
+    aria-labelledby="contact-tab"
+  >
+    <div class="card pt-2" style="box-shadow: none">
+      <div class="card-body text-center">
+        <div
+          class="d-flex flex-column justify-content-center align-items-center"
+          style="height: 300px"
+        >
+          <h1 style="font-size: 150%">No events recorded for this trip</h1>
+          <p class="text-muted">Drive with the app to see your events.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  mounted() {},
+};
+</script>
