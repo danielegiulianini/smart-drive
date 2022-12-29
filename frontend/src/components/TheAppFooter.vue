ca70c7565d4cac6735b9eab9d1ae62b6e5f5d466
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-29 23:19:36 +0100

    add footer component

---
+++ frontend/src/components/TheAppFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright mb-sm-4 mb-lg-0">
+      &copy; Copyright <strong><span>unibo</span></strong
+      >. All Rights Reserved
+    </div>
+  </footer>
+  <!-- End Footer -->
+</template>
+
+<script></script>
+
+<style scoped>
+/*--------------------------------------------------------------
+# Footer
+--------------------------------------------------------------*/
+.footer {
+  padding: 20px 0;
+  font-size: 14px;
+  transition: all 0.3s;
+  border-top: 1px solid #cddfff;
+}
+
+.footer .copyright {
+  text-align: center;
+  color: #012970;
+}
+
+.footer .credits {
+  padding-top: 5px;
+  text-align: center;
+  font-size: 13px;
+  color: #012970;
+}
+</style>
