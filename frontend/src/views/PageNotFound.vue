8d8909be0853404381af65cc1bf31340c7792f9a
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2022-12-30 15:03:58 +0100

    add not found page

---
+++ frontend/src/views/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <div class="container">
+      <section
+        class="section error-404 min-vh-50 d-flex flex-column align-items-center justify-content-center"
+      >
+        <h1>404</h1>
+        <h2>The page you are looking for doesn't exist.</h2>
+        <!--<a class="btn btn-light" href="/home">Back to home</a>-->
+        <img
+          src="/src/assets/img/dead-end.png"
+          class="img-fluid py-5"
+          alt="Page Not Found"
+          style="max-height: 500px"
+        />
+      </section>
+    </div>
+  </main>
+</template>
