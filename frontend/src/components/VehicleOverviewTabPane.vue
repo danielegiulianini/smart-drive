e616e98653eaaf597b76812937573856d2974738
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-01 00:07:56 +0100

    refactor vehicle overview tab pane making statistis item more user friendly

--- frontend/src/components/VehicleOverviewTabPane.vue
+++ frontend/src/components/VehicleOverviewTabPane.vue
@@ -1,18 +1,13 @@
 <template>
   <div
-    class="tab-pane fade show active"
+    class="tab-pane"
+    :class="{ active: isActive }"
     id="bordered-justified-home"
     role="tabpanel"
     aria-labelledby="home-tab"
   >
     <div class="card p-2 m-2" style="box-shadow: none">
-      <!-- QUI CI VA LA NUOVA ROW DEVE ESSERE ANCHE UNA CARD??-->
       <div class="card-body">
-        <!--TITOLO BELLO MA NON SO COSA SCRIVERCI!
-                            <h5 class="card-title">
-                            Accordion without outline borders
-                          </h5>-->
-
         <!-- Accordion without outline borders -->
         <div class="accordion accordion-flush" id="accordionFlushExample">
           <div class="accordion-item">
@@ -22,7 +17,7 @@
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#flush-collapseOne"
-                aria-expanded="false"
+                aria-expanded="true"
                 aria-controls="flush-collapseOne"
               >
                 <h1 style="font-size: 120%; font-family: Poppins, sans-serif">
@@ -37,63 +32,48 @@
               data-bs-parent="#accordionFlushExample"
             >
               <div class="accordion-body">
-                <!--In quelli grandi va su due colonne-->
-                <!-- List group With Icons -->
+                <!-- could have used a v-for if using array instead of object for vehicle's properties-->
                 <ul class="list-group list-group-flush">
-                  <li
-                    class="list-group-item d-flex justify-content-between align-middle"
-                  >
-                    <div>
-                      <span
-                        class="card-text text-muted"
-                        style="line-height: 50%; font-size: 90%"
-                      >
-                        year
-                      </span>
-                      <span class="card-text text-bf" style="font-size: 110%"
-                        >2020</span
-                      >
-                    </div>
+                    <TripDetailItem
+                      label="year"
+                      :itemValue="overview.year"
+                    ></TripDetailItem>
 
-                    <div>
-                      <span class="card-text text-bf" style="font-size: 105%"
-                        >Automatic</span
-                      >
-                      <span
-                        class="card-text text-muted"
-                        style="line-height: 50%; font-size: 90%"
-                      >
-                        trany
-                      </span>
-                    </div>
-                  </li>
-                  <li class="list-group-item">
-                    <div class="d-flex justify-content-between align-middle">
-                      <div>
-                        <span
-                          class="card-text text-muted"
-                          style="line-height: 50%; font-size: 90%"
-                        >
-                          year
-                        </span>
-                        <span class="card-text text-bf" style="font-size: 110%"
-                          >2020</span
-                        >
-                      </div>
+                    <TripDetailItem
+                      label="make"
+                      :itemValue="overview.make"
+                    ></TripDetailItem>
+                    <TripDetailItem
+                      label="model"
+                      :itemValue="overview.model"
+                    ></TripDetailItem>
 
-                      <div>
-                        <span class="card-text text-bf" style="font-size: 105%"
-                          >Automatic</span
-                        >
-                        <span
-                          class="card-text text-muted"
-                          style="line-height: 50%; font-size: 90%"
-                        >
-                          trany
-                        </span>
-                      </div>
-                    </div>
-                  </li>
+                    <TripDetailItem
+                      label="cylinders"
+                      :itemValue="overview.cylinders"
+                    ></TripDetailItem>
+                    <TripDetailItem
+                      label="trany"
+                      :itemValue="overview.trany"
+                    ></TripDetailItem>
+
+                    <TripDetailItem
+                      label="drive"
+                      :itemValue="overview.drive"
+                    ></TripDetailItem>
+                    <TripDetailItem
+                      label="EPA vehicle size class"
+                      :itemValue="overview.VClass"
+                    ></TripDetailItem>
+
+                    <TripDetailItem
+                      label="startStop"
+                      :itemValue="overview.startStop"
+                    ></TripDetailItem>
+                    <TripDetailItem
+                      label="Vehicle identification number"
+                      :itemValue="overview.vin"
+                    ></TripDetailItem>
                 </ul>
               </div>
             </div>
@@ -108,146 +88,117 @@
                 aria-expanded="false"
                 aria-controls="flush-collapseTwo"
               >
-                <div style="font-size: 120%" class="fw-bold">Emissions</div>
+                <div style="font-size: 120%" class="fw-bold">Fuel-related</div>
               </button>
             </h2>
             <div
               id="flush-collapseTwo"
               class="accordion-collapse collapse"
               aria-labelledby="flush-headingTwo"
-              data-bs-parent="#accordionFlushExample"
+              data-bs-parent="#accordionFlushExample2"
             >
               <ul class="list-group list-group-flush">
-                <li
-                  class="list-group-item d-flex justify-content-between align-middle"
-                >
-                  <div>
-                    <span
-                      class="card-text text-muted"
-                      style="line-height: 50%; font-size: 90%"
-                    >
-                      year
-                    </span>
-                    <span class="card-text text-bf" style="font-size: 110%"
-                      >2020</span
-                    >
-                  </div>
+                <!--
 
-                  <div>
-                    <span class="card-text text-bf" style="font-size: 105%"
-                      >Automatic</span
-                    >
-                    <span
-                      class="card-text text-muted"
-                      style="line-height: 50%; font-size: 90%"
-                    >
-                      trany
-                    </span>
-                  </div>
-                </li>
-                <li class="list-group-item">
-                  <div class="d-flex justify-content-between align-middle">
-                    <div>
-                      <span
-                        class="card-text text-muted"
-                        style="line-height: 50%; font-size: 90%"
-                      >
-                        year
-                      </span>
-                      <span class="card-text text-bf" style="font-size: 110%"
-                        >2020</span
-                      >
-                    </div>
+                  this.overview.barrels08 = vehicleModel.barrels08; //for fuel type 1
+        this.overview.comb08 = vehicleModel.comb08;
+        this.overview.highway08 = vehicleModel.highway08;
+        this.overview.city08 = vehicleModel.city08;
+        this.overview.fuelType = vehicleModel.fuelType;
+        this.overview.fuelCost08 = vehicleModel.fuelCost08; //annual (based on 15,000 miles) fuel cost (for fuel type 1)
+        this.overview.youSaveSpend = vehicleModel.youSaveSpend;
+                -->
+                  <TripDetailItem
+                    label="Combined Miles Per Gallon"
+                    :itemValue="overview.comb08"
+                  ></TripDetailItem>
 
-                    <div>
-                      <span class="card-text text-bf" style="font-size: 105%"
-                        >Automatic</span
-                      >
-                      <span
-                        class="card-text text-muted"
-                        style="line-height: 50%; font-size: 90%"
-                      >
-                        trany
-                      </span>
-                    </div>
-                  </div>
-                </li>
+                  <TripDetailItem
+                    label="Miles Per Gallon for highways"
+                    :itemValue="overview.highway08"
+                  ></TripDetailItem>
+                  <TripDetailItem
+                    label="Miles Per Gallon for city"
+                    :itemValue="overview.city08"
+                  ></TripDetailItem>
+
+                  <TripDetailItem
+                    label="fuel Type"
+                    :itemValue="overview.fuelType"
+                  ></TripDetailItem>
+                  <TripDetailItem
+                    label="Estimated annual fuel cost"
+                    :itemValue="overview.fuelCost08"
+                  ></TripDetailItem>
+
+                  <TripDetailItem
+                    label="startStop"
+                    :itemValue="overview.startStop"
+                  ></TripDetailItem>
+                  <TripDetailItem
+                    label="save/spend in 5 years compared to an average car ($)"
+                    :itemValue="overview.youSaveSpend"
+                  ></TripDetailItem>
               </ul>
-              <!--</div>-->
-              <!--end first col-->
-              <!--<div class="col-md-6">
-                                    <ul class="list-group">
-                                      <li class="list-group-item">
-                                        <i
-                                          class="bi bi-star me-1 text-success"
-                                        ></i>
-                                        <span class="text-muted">year </span
-                                        ><span class="fw-bold">2022</span>
-                                      </li>
-                                      <li class="list-group-item">
-                                        <i
-                                          class="bi bi-collection me-1 text-primary"
-                                        ></i>
-                                        model
-                                      </li>
-                                      <li class="list-group-item">
-                                        <i
-                                          class="bi bi-check-circle me-1 text-danger"
-                                        ></i>
-                                        make
-                                      </li>
-                                      <li class="list-group-item">
-                                        <i
-                                          class="bi bi-exclamation-octagon me-1 text-warning"
-                                        ></i>
-                                        ecoscore 8/10
-                                      </li>
-                                    </ul>
-                                  </div>-->
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button
+                class="accordion-button collapsed fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseThree"
+                aria-expanded="false"
+                aria-controls="flush-collapseThree"
+              >
+                <div style="font-size: 120%" class="fw-bold">Emissions</div>
+              </button>
+            </h2>
+            <div
+              id="flush-collapseThree"
+              class="accordion-collapse collapse"
+              aria-labelledby="flush-headingThree"
+              data-bs-parent="#accordionFlushExample3"
+            >
+              <ul class="list-group list-group-flush">
+                  <TripDetailItem
+                    label="tailpipe CO2 in grams/mile"
+                    :itemValue="overview.co2"
+                  ></TripDetailItem>
+
+                  <TripDetailItem
+                    label="tailpipe CO2 in grams/mile"
+                    :itemValue="overview.co2TailpipeGpm"
+                  ></TripDetailItem>
+              </ul>
               <!--end first col-->
             </div>
           </div>
         </div>
+        <!-- End Accordion without outline borders -->
       </div>
-      <!-- End Accordion without outline borders -->
+      <!-- End of card body -->
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-
-import TheAppHeader from "./TheAppHeader.vue";
-import TheAppSidebar from "./TheAppSidebar.vue";
-import TheAppFooter from "./TheAppFooter.vue";
-
+import TripDetailItem from "./TripDetailItem.vue";
 export default {
-  components: {
-    TheAppHeader,
-    TheAppSidebar,
-    TheAppFooter,
-  },
+  inheritAttrs: false,
+  components: { TripDetailItem },
   props: {
-    progressPercentage: {
-      type: Number, // Number from 0.0 to 1.0
+    isActive: {
+      type: Boolean,
       required: true,
     },
-  },
-  data() {
-    //this cannot be a field because every instance of this component must have its own state
-    return {
-      author: {
-        name: "John Doe",
-        books: [
-          "Vue 2 - Advanced Guide",
-          "Vue 3 - Basic Guide",
-          "Vue 4 - The Mystery",
-        ],
-      },
-    };
+    overview: {
+      type: Object,
+      required: true,
+    },
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
