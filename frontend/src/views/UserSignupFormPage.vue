0784e63754dfe2c76104f763508e9f59785c4643
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-01 17:08:09 +0100

    fix width of signup page

---
+++ frontend/src/views/UserSignupFormPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <spinner :show1="true"></spinner>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div
+        class="col-lg-5 col-md-8 d-flex flex-column align-items-center justify-content-center"
+      >
+        <UserSignupForm></UserSignupForm>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserSignupForm from "../components/UserSignupForm.vue";
+import Spinner from "../components/Spinner.vue";
+
+export default {
+  components: {
+    UserSignupForm,
+    Spinner,
+  },
+};
+</script>
+
+<style scoped></style>
