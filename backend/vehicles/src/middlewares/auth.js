fd9e6f9c6732590e5e9c04760536198b0f9c6923
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-02 17:20:17 +0100

    fix all auth middlewares

--- backend/vehicles/src/middlewares/auth.js
+++ backend/vehicles/src/middlewares/auth.js
@@ -10,10 +10,10 @@ const supabase = createClient.createClient(
 //backend copy token from headers to body off client request
 const extractUserIdFromTokenAndPutItToBody = async (req, res, next) => {
   let error = "";
-
+  console.log("auth middleware in action");
   if (req.headers.authorization) {
     //retrieve access_token from request
-    let access_token = req.headers.authorization;
+    /*let access_token = req.headers.authorization;
     supabase.auth.api.getUser(access_token).catch((err) => {
       req.body.tokenUserId = null;
       error = err;
@@ -21,15 +21,30 @@ const extractUserIdFromTokenAndPutItToBody = async (req, res, next) => {
     if (req.body.tokenUserId) {
       next();
       return;
-    }
-  }
+    }*/
 
-  //if here no token has been found (catching 1. bad format and 2. userId mismatch here)
-  res.status(401).json({
-    message: "You must be authenticated!!",
-    details: error,
-    code: 401,
-  });
+    access_token = req.headers.authorization;
+    supabase.auth
+      .getUser(access_token)
+      .then((user) => (req.body.userId = user.id))
+      .catch((err) => {
+        req.body.userId = null;
+        error = err;
+      })
+      .then(() => {
+        if (req.body.userId) {
+          next();
+          return;
+        } else {
+          //if here no token has been found (catching 1. bad format and 2. userId mismatch here)
+          res.status(401).json({
+            message: "You must be authenticated!!",
+            details: error,
+            code: 401,
+          });
+        }
+      });
+  }
 };
 
 module.exports = {
