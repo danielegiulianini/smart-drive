e508676630b2439690ea215eea578ace91979aa6
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-03 11:31:05 +0100

    add last configs to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8000,	//should read it from docker-compose's env (shared with backend)
+    port: 8000, //should read it from docker-compose's env (shared with backend)
   },
-})
+  commonjsOptions: {
+    esmExternals: true,
+  },
+  //for google maps (see https://github.com/fawmi/vue-google-maps/issues/148#issuecomment-1235143844)
+  optimizeDeps: {
+    include: ["@fawmi/vue-google-maps", "fast-deep-equal"],
+  },
+});
