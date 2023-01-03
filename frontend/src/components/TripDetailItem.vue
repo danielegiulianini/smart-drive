2c168ce889d5d9d17849de1396349523654b1b93
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-03 11:29:33 +0100

    add trip detial item

---
+++ frontend/src/components/TripDetailItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="list-group-item d-flex justify-content-between align-middle">
+    <div class="mx-auto">
+      <span
+        class="card-text text-muted pe-1 text-center"
+        style="line-height: 50%; font-size: 90%"
+        >{{ label }}</span
+      ><span class="card-text text-bf" style="font-size: 110%">{{
+        itemValue
+      }}</span
+      ><!--v-if-->
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { required: true },
+    itemValue: { required: true },
+  },
+};
+</script>
