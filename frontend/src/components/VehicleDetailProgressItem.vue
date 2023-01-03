897090b6104119aeb9695a52537c6f3f8a132298
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-03 11:27:40 +0100

    add vehicle detail progress item

---
+++ frontend/src/components/VehicleDetailProgressItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span
+      class="card-text text-muted pe-1"
+      style="line-height: 50%; font-size: 90%"
+      v-if="!right"
+    >
+      {{ label }}
+    </span>
+    <span class="card-text text-bf" style="font-size: 110%">{{ value }}</span>
+    <span
+      class="card-text text-muted ps-1"
+      style="line-height: 50%; font-size: 90%"
+      v-if="right"
+    >
+      {{ label }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { required: true },
+    value: { required: true }, //assumptions all default values are set to zero
+    right: { default: false },
+  },
+};
+</script>
