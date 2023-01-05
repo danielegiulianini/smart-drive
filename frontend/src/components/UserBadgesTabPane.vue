dbdbb25a71396f01ac91e3b59a49567887509c78
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-05 11:55:17 +0100

    add no badges for userbadges pane

--- frontend/src/components/UserBadgesTabPane.vue
+++ frontend/src/components/UserBadgesTabPane.vue
@@ -9,53 +9,64 @@
     <div class="container-fluid">
       <div class="card-body pb-0 mb-0 ps-1">
         <!-- could have used accordion too-->
-        <div class="d-flex justify-content-between">
-          <div class="card-title pb-0 mb-0 ms-2 mt-0">Achieved</div>
-          <div class="filter">
-            <a
-              class="icon"
-              href="#"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-              ><i class="bi bi-question-circle"></i
-            ></a>
-            <ul
-              class="dropdown-menu dropdown-menu-end dropdown-menu-arrow"
-              style=""
-            >
-              <li>
-                <a class="dropdown-item" href="#" route>
-                  <router-link to="allBadges">See all</router-link>
-                </a>
-              </li>
-              <!--<li><a class="dropdown-item" href="#">This Month</a></li>
+        <div class="filter">
+          <a
+            class="icon"
+            href="#"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            ><i class="bi bi-question-circle"></i
+          ></a>
+          <ul
+            class="dropdown-menu dropdown-menu-end dropdown-menu-arrow"
+            style=""
+          >
+            <li>
+              <a class="dropdown-item" href="#" route>
+                <router-link to="allBadges">See all</router-link>
+              </a>
+            </li>
+            <!--<li><a class="dropdown-item" href="#">This Month</a></li>
               <li><a class="dropdown-item" href="#">This Year</a></li>-->
-            </ul>
-          </div>
+          </ul>
         </div>
-        <div class="row row-cols-2 row-cols-md-4 g-1">
-          <div class="col" v-for="(badge, index) in myBadges">
-            <UserBadge
-              :key="badge.id"
-              :name="
-                badges.filter((badgeDetail) => badgeDetail.id == badge.id).name
-              "
-              :description="
-                badges.filter((badgeDetail) => badgeDetail.id == badge.id)
-                  .description
-              "
-              :pictureUri="
-                badges.filter((badgeDetail) => badgeDetail.id == badge.id)
-                  .pictureUri
-              "
-            ></UserBadge>
+        <div v-if="myBadges.length > 0">
+          <div class="d-flex justify-content-between">
+            <div class="card-title pb-0 mb-0 ms-2 mt-0">Achieved</div>
           </div>
-          <!--<div class="col"><UserBadge></UserBadge></div>
+
+          <div class="row row-cols-2 row-cols-md-4 g-1">
+            <div class="col" v-for="(badge, index) in myBadges">
+              <UserBadge
+                :key="badge.id"
+                :name="
+                  badges.filter((badgeDetail) => badgeDetail.id == badge.id)
+                    .name
+                "
+                :description="
+                  badges.filter((badgeDetail) => badgeDetail.id == badge.id)
+                    .description
+                "
+                :pictureUri="
+                  badges.filter((badgeDetail) => badgeDetail.id == badge.id)
+                    .pictureUri
+                "
+              ></UserBadge>
+            </div>
+            <!--<div class="col"><UserBadge></UserBadge></div>
           <div class="col"><UserBadge></UserBadge></div>
           <div class="col"><UserBadge></UserBadge></div>-->
+          </div>
+          <div class="card-title pb-0 mb-0 ms-2 mt-0">Achieving</div>
         </div>
-        <div class="card-title pb-0 mb-0 ms-2 mt-0">Achieving</div>
-        
+        <div
+          v-else
+          class="d-flex flex-column justify-content-center align-items-center"
+          style="height: 300px"
+        >
+          <h1 style="font-size: 150%">Still no achievements</h1>
+          <p class="text-muted">Use the app to gain badges.</p>
+        </div>
       </div>
     </div>
   </div>
@@ -71,7 +82,7 @@ import UserBadge from "./UserBadge.vue";
 import badges from "../mixins/badges.vue";
 
 export default {
-  components: { UserBadge, },
+  components: { UserBadge },
   props: {
     isActive: {
       type: Boolean,
