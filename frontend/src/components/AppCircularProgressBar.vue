222561692a6d16bd0e852a7c7d567ef44215ff5f
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-09 17:08:12 +0100

    make frontend\src\components\AppCircularProgressBar reactive (when score of user is retrieved

--- frontend/src/components/AppCircularProgressBar.vue
+++ frontend/src/components/AppCircularProgressBar.vue
@@ -3,13 +3,18 @@
     <div
       ref="container"
       class="d-flex my-auto mx-auto"
-      style="width: 200px; height: 200px"
+      style="width: 230px; height: 230px"
     ></div>
     <div
-      style="position: relative; top: -18vh; font-size: 250%"
+      style="
+        position: relative;
+        top: -142px;
+        margin-bottom: -60px;
+        font-size: 250%;
+      "
       class="text-center"
     >
-      <div style="line-height: 100%">100</div>
+      <div style="line-height: 100%">{{ formattedValue }}</div>
       <div class="text-muted" style="font-size: 70%; line-height: 90%">
         /100
       </div>
@@ -22,49 +27,78 @@ import ProgressBar from "progressbar.js";
 
 export default {
   props: {
-    progressPercentage: {
-      type: Number, // Number from 0.0 to 1.0
+    outOf: {
+      default: 100,
+    },
+    progress: {
       required: true,
     },
   },
+  computed: {
+    progressPercentage() {
+      return this.progress ? this.progress / this.outOf : this.progress; // Number from 0.0 to 1.0
+    },
+    formattedValue() {
+      const decimalDigitsCount = 2;
+      return this.progress
+        ? this.progress.toFixed(decimalDigitsCount)
+        : this.progress;
+    },
+  },
+  methods: {
+    isDefined(variable) {
+      return !(typeof variable === "undefined" || variable === null);
+    },
+  },
   mounted() {
-    var htmlBar = this.$refs.container;
-    console.log("la bar is", htmlBar);
-    htmlBar = new ProgressBar.Circle(htmlBar, {
-      strokeWidth: 7,
-      //easing: "easeInOut",
-      duration: 1400,
-      //color: "#FF0000",
-      from: { color: "#FF0000" },
-      to: { color: "#00FF00" },
-      step: function (state, circle, attachment) {
-        circle.path.setAttribute("stroke", state.color);
-      },
+    console.log("il progress in ciruclar app is", this.progress);
 
-      trailColor: "#eee",
-      trailWidth: 1,
-      svgStyle: null,
-      text: {
-        style: {
-          fontFamily: "Poppins",
-          color: "#f00",
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          padding: 0,
-          margin: 0,
-          // You can specify styles which will be browser prefixed
-          transform: {
-            prefix: true,
-            value: "translate(-50%, -50%)",
+    console.log(
+      "il progressPercentage in ciruclar app is",
+      this.progressPercentage
+    );
+  },
+  watch: {
+    progressPercentage(newVal) {
+      if (this.isDefined(this.progressPercentage)) {
+        var htmlBar = this.$refs.container;
+        console.log("la bar is", htmlBar);
+        htmlBar = new ProgressBar.Circle(htmlBar, {
+          strokeWidth: 7,
+          //easing: "easeInOut",
+          duration: 1400,
+          //color: "#FF0000",
+          from: { color: "#FF0000" },
+          to: { color: "#00FF00" },
+          step: function (state, circle, attachment) {
+            circle.path.setAttribute("stroke", state.color);
           },
-        },
-      },
-    });
 
-    htmlBar.path.style.strokeLinecap = "round";
+          trailColor: "#eee",
+          trailWidth: 1,
+          svgStyle: null,
+          text: {
+            style: {
+              fontFamily: "Poppins",
+              color: "#f00",
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              padding: 0,
+              margin: 0,
+              // You can specify styles which will be browser prefixed
+              transform: {
+                prefix: true,
+                value: "translate(-50%, -50%)",
+              },
+            },
+          },
+        });
 
-    htmlBar.animate(this.progressPercentage); // Number from 0.0 to 1.0  },
+        htmlBar.path.style.strokeLinecap = "round";
+        htmlBar.animate(this.progressPercentage); // Number from 0.0 to 1.0
+      }
+    },
   },
 };
 </script>
