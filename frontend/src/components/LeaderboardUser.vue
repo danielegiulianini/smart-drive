5a083a91f90992d920deb9cbb940a98dd89fbff9
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-10 09:31:16 +0100

    adapt LeaderboardUser to new progress bar

--- frontend/src/components/LeaderboardUser.vue
+++ frontend/src/components/LeaderboardUser.vue
@@ -3,24 +3,20 @@
   <tr :class="rowClassObject">
     <td class="text-muted text-center">
       {{ rank
-      }}<i
-        class="bi bi-trophy"
-        style="color: silver"
-        v-if="rank < 4"
-        :class="trophyClass"
-      ></i>
+      }}<i class="bi bi-trophy" v-if="rank < 4" :class="trophyClassObject"></i>
+      <!--         style="color: silver"-->
     </td>
     <th scope="row align-text-center">
       <div class="d-flex">
         <a href="#" class="d-flex"
           ><img
-            :src="profilePictureUri"
+            :src="actualPictureUri"
             class="rounded-circle my-auto"
             style="max-width: 50px"
             alt=""
         /></a>
         <div class="ps-3 my-auto" style="font-size: 95%">
-          {{name surname}}
+          {{ firstName }} {{ surname }}
           <div class="text-muted" style="font-size: 95%">level {{ level }}</div>
         </div>
       </div>
@@ -32,7 +28,7 @@
     </td>
     <td class="fw-bold my-auto text-center">
       <AppSemiCircularProgressBar
-        :progressPercentage=score
+        :progress="score"
       ></AppSemiCircularProgressBar>
     </td>
   </tr>
@@ -40,6 +36,7 @@
 
 <script>
 import AppSemiCircularProgressBar from "./AppSemiCircularProgressBar.vue";
+const defaultAvatarUri = "/src/assets/img/driverAvatar.png";
 
 export default {
   components: {
@@ -56,53 +53,34 @@ export default {
     badges: { required: true },
   },
   computed: {
+    actualPictureUri() {
+      return this.profilePictureUri ? this.profilePictureUri : defaultAvatarUri;
+    },
     badgesCount() {
       return this.badges.length;
     },
     rowClassObject() {
       return {
-        "gold-row": rank == 1,
-        "silver-row": rank == 2,
-        "bronze-row": rank == 3,
+        "gold-row": this.rank == 1,
+        "silver-row": this.rank == 2,
+        "bronze-row": this.rank == 3,
       };
     },
     trophyClassObject() {
       return {
-        "gold-trophy": rank == 1,
-        "silver-trophy": rank == 2,
-        "bronze-trophy": rank == 3,
+        "gold-trophy": this.rank == 1,
+        "silver-trophy": this.rank == 2,
+        "bronze-trophy": this.rank == 3,
       };
     },
   },
-  methods: {
-    timeSince(date) {
-      console.log("la date to whuch say timeSince", date);
-      var seconds = Math.floor((new Date() - date) / 1000);
-
-      var interval = seconds / 31536000;
-
-      if (interval > 1) {
-        return Math.floor(interval) + " years";
-      }
-      interval = seconds / 2592000;
-      if (interval > 1) {
-        return Math.floor(interval) + " months";
-      }
-      interval = seconds / 86400;
-      if (interval > 1) {
-        return Math.floor(interval) + " days";
-      }
-      interval = seconds / 3600;
-      if (interval > 1) {
-        return Math.floor(interval) + " hours";
-      }
-      interval = seconds / 60;
-      if (interval > 1) {
-        return Math.floor(interval) + " minutes";
-      }
-      return Math.floor(seconds) + " seconds";
+  methods: {},
+  watcher: {
+    profilePictureUri(newUri, oldUri) {
+      console.log("il mio profilePictureUri", newUri);
     },
   },
+  mounted() {},
 };
 </script>
 
@@ -115,20 +93,24 @@ export default {
 }
 
 .bronze-trophy {
-  color: bronze;
+  color: #cd7f32;
 }
 
-.silver-row {
+.gold-row {
   border-color: gold;
   border-width: 1px 1px;
+  z-index: 3;
 }
 
 .silver-row {
   border-color: silver;
   border-width: 1px 1px;
+  z-index: 2;
 }
-.silver-row {
-  border-color: bronze;
+.bronze-row {
+  border-color: #cd7f32;
   border-width: 1px 1px;
+  border-bottom: 0.5px solid #cd7f32;
+  z-index: 1;
 }
 </style>
