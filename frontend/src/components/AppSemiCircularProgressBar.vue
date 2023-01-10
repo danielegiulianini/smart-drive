17015761f4a1c23f548090bd7c9f06b34689d4ec
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-10 09:30:23 +0100

    make semi circular bars reactive

--- frontend/src/components/AppSemiCircularProgressBar.vue
+++ frontend/src/components/AppSemiCircularProgressBar.vue
@@ -2,11 +2,11 @@
   <div>
     <div
       ref="container"
-      class="d-flex my-auto"
-      style="width: 70px; height: 70px"
+      class="d-flex my-auto mx-auto"
+      style="width: 50px; height: 50px"
     ></div>
-    <div style="position: relative; top: -30px">
-      <div style="line-height: 90%">100</div>
+    <div style="position: relative; top: -23px">
+      <div style="line-height: 90%">{{ formattedValue }}</div>
       <div style="font-size: 80%; line-height: 90%" class="text-muted">
         /100
       </div>
@@ -19,49 +19,72 @@ import ProgressBar from "progressbar.js";
 
 export default {
   props: {
-    progressPercentage: {
-      type: Number, // Number from 0.0 to 1.0
+    outOf: {
+      default: 100,
+    },
+    progress: {
       required: true,
     },
   },
-  mounted() {
-    var htmlBar = this.$refs.container;
-    console.log("la bar is",htmlBar )
-    htmlBar = new ProgressBar.SemiCircle(htmlBar, {
-      strokeWidth: 7,
-      //easing: "easeInOut",
-      duration: 1400,
-      //color: "#FF0000",
-      from: { color: "#FF0000" },
-      to: { color: "#00FF00" },
-      step: function (state, circle, attachment) {
-        circle.path.setAttribute("stroke", state.color);
-      },
+  computed: {
+    progressPercentage() {
+      return this.progress ? this.progress / this.outOf : this.progress; // Number from 0.0 to 1.0
+    },
+    formattedValue() {
+      const decimalDigitsCount = 2;
+      return this.progress
+        ? this.progress.toFixed(decimalDigitsCount)
+        : this.progress;
+    },
+  },
+  data() {
+    return { htmlBar: "" };
+  },
+  methods: {
+    initBar() {
+      var htmlBar = this.$refs.container;
+      this.htmlBar = new ProgressBar.SemiCircle(htmlBar, {
+        strokeWidth: 7,
+        //easing: "easeInOut",
+        duration: 1400,
+        //color: "#FF0000",
+        from: { color: "#FF0000" },
+        to: { color: "#00FF00" },
+        step: function (state, circle, attachment) {
+          circle.path.setAttribute("stroke", state.color);
+        },
 
-      trailColor: "#eee",
-      trailWidth: 1,
-      svgStyle: null,
-      text: {
-        style: {
-          fontFamily: "Poppins",
-          color: "#f00",
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          padding: 0,
-          margin: 0,
-          // You can specify styles which will be browser prefixed
-          transform: {
-            prefix: true,
-            value: "translate(-50%, -50%)",
+        trailColor: "#eee",
+        trailWidth: 1,
+        svgStyle: null,
+        text: {
+          style: {
+            fontFamily: "Poppins",
+            color: "#f00",
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            padding: 0,
+            margin: 0,
+            // You can specify styles which will be browser prefixed
+            transform: {
+              prefix: true,
+              value: "translate(-50%, -50%)",
+            },
           },
         },
-      },
-    });
+      });
 
-    htmlBar.path.style.strokeLinecap = "round";
-
-    htmlBar.animate(this.progressPercentage); // Number from 0.0 to 1.0  },
+      this.htmlBar.path.style.strokeLinecap = "round";
+    },
+  },
+  mounted() {
+    this.initBar();
+  },
+  watch: {  //maybe an eader watcher should be used
+    progressPercentage(newVal) {
+      this.htmlBar.animate(newVal); // Number from 0.0 to 1.0
+    },
   },
 };
 </script>
