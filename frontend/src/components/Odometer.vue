2139b9621a8609f1dc7d8e6bce9e5ae92a4c4024
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-17 15:19:48 +0100

    fix odometer reactivity

--- frontend/src/components/Odometer.vue
+++ frontend/src/components/Odometer.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
-    <div style="font-size: 200%; line-height: 90%" class="pt-4">10000.00</div>
+    <div style="font-size: 200%; line-height: 90%" class="pt-4 text-center">
+      {{ tweened }}
+    </div>
     <div class="text-muted text-center" style="font-size: 105%">km</div>
-    <!--&lt;Vue3Odometer :value=&quot;num&quot; /&gt;-->
   </div>
 </template>
 
 <script>
+import gsap from "gsap";
+
 export default {
   props: {
-    rpm: {
+    odometer: {
       required: true,
     },
   },
+  data() {
+    return {
+      tweened: this.odometer,
+    };
+  },
   mounted() {},
+  watch: {
+    odometer(newOdometer) {
+      console.log("odometer changed!!")
+      gsap.to(this, { duration: 0.5, tweened: Number(newOdometer) || 0 });
+    },
+  },
 };
 </script>
 
