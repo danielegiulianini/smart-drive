48a0ba0ec5a8fea0049cd0bb3b54e7284b4ae378
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-01-18 10:56:06 +0100

    fix double badges assignment

--- backend/users/src/services/achievements.js
+++ backend/users/src/services/achievements.js
@@ -12,24 +12,33 @@ const User = require("../models/users");
 
 //it returns the achievements unlocked
 async function unlockAchievements(userIds, achievements) {
-  let unlockedAchievements = [];
-  if (userIds.length > 0) {
-    let users = await User.find({
-      _id: { $in: userIds },
-    });
-    for (let user of users) {
-      for (let achievement of achievements) {
-        if (!user.unlockedAchievements.includes(achievement)) {
-          unlockedAchievements.push(achievement);
-          //user.unlockedAchievements.push(achievement);
+  let newUnlockedAchievements = [];
+
+  try {
+    console.log("achievements to unlock: ", achievements);
+    if (userIds.length > 0) {
+      let users = await User.find({
+        _id: { $in: userIds },
+      });
+      for (let user of users) {
+        for (let achievement of achievements) {
+          if (
+            !user.unlockedAchievements
+              .map((achiev) => achiev.id)
+              .includes(achievement)
+          ) {
+            newUnlockedAchievements.push({ id: achievement });
+          }
         }
+        user.unlockedAchievements.push(...newUnlockedAchievements); //push doesn't take array as argument, but only variable length arg (leveraging spread operator for this)
+        await user.save();
       }
-      user.unlockedAchievements = unlockedAchievements;
-      await user.save();
+      //not putting publish here but at higher level to ease testing (no mocking needed)
     }
-    //not putting publish here but at higher level to ease testing (no mocking needed)
+  } catch (error) {
+    console.log("error at achievements of users is: ", error);
   }
-  return unlockedAchievements;
+  return newUnlockedAchievements;
 }
 
 module.exports = {
