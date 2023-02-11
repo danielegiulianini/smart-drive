8077e58df9cf9c71724d1b50d068cc5ee3a7efd5
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2023-02-11 21:32:42 +0100

    add trips and user performances

---
+++ performanceTest/tripsPerformanceTest.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from locust import HttpUser, task, between
+from random_utils import get_random_string
+import json
+
+class UsersPerformanceTest(HttpUser):
+    host = "http://192.168.8.103:8082/api/v1/trips"    # or (if on the same platform) "http://localhost:8082/api/v1/users"
+    wait_time = between(1, 5)  # wait time between every task
+
+    # run for each simulated user
+    def on_start(self):
+        # add
+        tripInButes = self.client.post(
+                "/", json={"sensorId": get_random_string(10)}).content
+        trip_json = tripInButes.decode('utf8').replace("'", '"')
+        self.trip = json.loads(trip_json)
+
+    @task
+    def CRUD(self):
+        # list
+        self.client.get("/")
+        # get
+        self.client.get("/"+ self.trip["_id"], name="/trips/:id")
+
+    def on_stop(self):
+        # close trip
+        self.client.post("/"+self.trip["_id"], json={"sensorId": "bar"},  name="/trips/:id")        # required data only
--- performanceTest/usersPerformanceTest.py
+++ performanceTest/usersPerformanceTest.py
@@ -5,21 +5,25 @@ from random_utils import get_random_string
 
 
 class UsersPerformanceTest(HttpUser):
-    host = "http://localhost:8082/api/v1/users"
+    host = "http://192.168.8.103:8082/api/v1/users"    # or (if on the same platform) "http://localhost:8082/api/v1/users"
     wait_time = between(1, 5)  # wait time between every task
-    userId = get_random_string(10)
 
     # run for each simulated user
     def on_start(self):
         # add
+        self.userId = get_random_string(10)
+        self.email = get_random_string(10)
         self.client.post(
-            "/", json={"_id": self.userId, "name": "bar", "surname": "bar"})
+            "/", json={"tokenUserId": self.userId, "name": "bar", "surname": "bar", "email": self.email})
+        print("printing in sequence userId , srunmae e mail")
+        print(self.userId)
+        print(self.email)
 
     @task
     def CRUD(self):
         # edit
         self.client.post(
-            "/"+self.userId, json={"_id": "foo", "name": "bar", "surname": "bar"},  name="/users/:id")        # required data only
+            "/"+self.userId, json={"name": "bar", "surname": "bar"},  name="/users/:id")        # required data only
         # list
         self.client.get("/")
         # get
